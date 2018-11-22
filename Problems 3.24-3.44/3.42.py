84538a153eabd2eac6a71e1fa1b1195578ee0f90
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:19:43 +0500

    Delete 3.42.py

--- Problems 3.24-3.44/3.42.py
+++
@@ -1,9 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.42 ")
-
-def avg(numbers):
-    for i in numbers:
-        b = [sum(i) / len(i)]
-        print(*b)
-
