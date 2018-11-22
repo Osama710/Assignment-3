3b0874480163a9d1fd7b94369fdd40a28ef67d69
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:16:18 +0500

    Delete 3.26.py

--- Problems 3.24-3.44/3.26.py
+++
@@ -1,7 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.26 ")
-
-lst = eval(input("Enter a non empty list: "))
-print("The first element of the list is: ", lst[0])
-print("Te last element of the list is: ", lst[-1])
