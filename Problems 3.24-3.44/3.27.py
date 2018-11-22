d3177e1a2d5109e6471cab05fee439989b8f2ed0
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:17:26 +0500

    Delete 3.27.py

--- Problems 3.24-3.44/3.27.py
+++
@@ -1,7 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.27 ")
-
-n = int(input("Enter a positive integer: "))
-for mul in range(0,n*4,n):
-    print(mul)
