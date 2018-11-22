53294e5db0b34c6b71ab0c4a1a128cc8a516093c
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:19:23 +0500

    Delete 3.40.py

--- Problems 3.24-3.44/3.40.py
+++
@@ -1,9 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.40 ")
-
-def partition(lst):
-    for x in lst:
-        x.casefold()
-        if x[1] in "abcdefghijklm":
-            print(x)
