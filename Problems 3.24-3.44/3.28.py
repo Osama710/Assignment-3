cd68bdd76c9fa56f2b1bc40ba62ff6609746b946
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:17:33 +0500

    Delete 3.28.py

--- Problems 3.24-3.44/3.28.py
+++
@@ -1,8 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.28 ")
-
-n = int(input("Enter an integer: "))
-for i in range(0,n):
-    if  i != n:
-        print(i**2)
