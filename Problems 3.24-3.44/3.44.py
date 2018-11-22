178b42283c73b7a1edd3eafe7d8b0b57f1f57f48
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:20:01 +0500

    Delete 3.44.py

--- Problems 3.24-3.44/3.44.py
+++
@@ -1,8 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Problem No: 3.44 ")
-
-def distance(time):
-    sound = 340.29*time
-    sol = sound/1000
-    return sol
