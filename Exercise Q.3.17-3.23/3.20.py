afd84881d3c623c1ca70b876401b9fdb1d0a94ae
Muhammad Osama <45150954+Osama710@users.noreply.github.com>
2018-11-22 22:22:11 +0500

    Delete 3.20.py

--- Exercise Q.3.17-3.23/3.20.py
+++
@@ -1,9 +0,0 @@
-print("Muhammad Osama - 18B-003-CS - Sec'A'")
-print("Detailed Assignment")
-print("Exercise Questions")
-print("Ex: 3.20")
-
-months = ['January','February','March']
-for char in months:
-    print(char[:3])
-    
